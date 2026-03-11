4a527ac72a72885f86f49de12c07082e7bd43c08
podnikatelka <4bachova.natalie@skolamichael.cz>
2026-03-11 08:37:03 +0100

    naty skript

--- Assets/Bachova.cs
+++ Assets/Bachova.cs
@@ -5,7 +5,7 @@ public class Bachova : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("ahoj, zdravím vsechny");
     }
 
     // Update is called once per frame
