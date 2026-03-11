8e3fc5a7161be86c96e6a2e48468a3f724bef1fd
esterka1 <4mandic.ester@skolamichael.cz>
2026-03-11 08:37:56 +0100

    POZDRAV

--- Assets/Mandic.cs
+++ Assets/Mandic.cs
@@ -5,7 +5,7 @@ public class Mandic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("NAZDAR TADY ESTER");
     }
 
     // Update is called once per frame
