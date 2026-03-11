07164f76f4689c4672a57bb9363147ea08278bba
Blubii <4knezova.johana@skolamichael.cz>
2026-03-11 08:41:25 +0100

    ahoj

--- Assets/Knezova.cs
+++ Assets/Knezova.cs
@@ -5,12 +5,13 @@ public class Knezova : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Dobry den jsem johy ");
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
     }
 }
