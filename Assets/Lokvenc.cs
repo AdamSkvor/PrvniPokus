60d7c14d2846ac6356f7c96273fcff268f45b929
GGlukyy <4lokvenc.lukas@skolamichael.cz>
2026-03-11 09:07:40 +0100

    Update LL
    změna řazení ve scriptu

--- Assets/Lokvenc.cs
+++ Assets/Lokvenc.cs
@@ -6,7 +6,7 @@ public class Lokvenc : MonoBehaviour
 void Start()
     {
         //Sem patří pozdrav
-       Debug.Log ("Ahoj - Lukáš Lokvenc")
+        Debug.Log ("Ahoj - Lukáš Lokvenc")
         //Sem patří pozdrav
     }
 
