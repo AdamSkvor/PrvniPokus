841197c029503e7877102951636d43055ab4a231
Tramvajka <4molnarova.julia@skolamichael.cz>
2026-03-11 08:40:23 +0100

    Ahoj from Jula

--- Assets/Molnarova.cs
+++ Assets/Molnarova.cs
@@ -5,7 +5,7 @@ public class Molnarova : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Ahoj tady Jula");
     }
 
     // Update is called once per frame
