0f880bba14a5255d17191def0ab3cc5b5555ce71
Scarcz576 <skvr.ad@gmail.com>
2026-03-11 08:35:52 +0100

    update

--- Assets/Subenko.cs
+++ Assets/Subenko.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class Subenko : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+void Start()
     {
-        
+        //Sem patří pozdrav
+        //Sem patří pozdrav
+        //Sem patří pozdrav
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Sem nepatří pozdrav
     }
 }
