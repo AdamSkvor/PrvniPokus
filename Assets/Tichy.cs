91eb45dd954e31de12456734a79527beee4b736d
xangelalex <4ticha.natalie@skolamichael.cz>
2026-03-11 08:41:56 +0100

    alexpredstaveni

--- Assets/Tichy.cs
+++ Assets/Tichy.cs
@@ -5,7 +5,7 @@ public class Tichy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       Debug.Log("Ahoj, jsem alex a jsem hlučný, ale to nevadí, protože jsem tichy"); 
     }
 
     // Update is called once per frame
