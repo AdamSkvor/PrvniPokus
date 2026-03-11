83d0cfa2a37d5b4f08b8d6610732a7aaac2b4930
Scarcz576 <skvr.ad@gmail.com>
2026-03-11 09:00:35 +0100

    Update Pozdrav.cs

--- Assets/Pozdrav.cs
+++ Assets/Pozdrav.cs
@@ -5,7 +5,7 @@ public class pohyb : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 void Start()
     {
-        Debug.Log("Všechny zdravím")
+        Debug.Log("Všechny zdravím");
     }
 
     // Update is called once per frame
